971e3692e68933bbb12480f1cfaa6915c322077d
bob-omboto <omboto@live.com>
2025-09-10 11:15:26 -0400

    Updated ingest_crypto file

--- src/ingest_crypto.py
+++ src/ingest_crypto.py
@@ -22,11 +22,11 @@ secret_client = SecretClient(vault_url=vault_url, credential=credential)
 eventhub_name = secret_client.get_secret(eventhub_name_secret).value
 connection_str = secret_client.get_secret(eventhub_string_secret).value
 
-eventhub_name = os.environ.get('eventhubname')
-eventhub_string = os.environ.get('eventhubstring')
+# eventhub_name = os.environ.get('eventhubname')
+# eventhub_string = os.environ.get('eventhubstring')
 
-if not eventhub_name or not eventhub_string:
-    raise ValueError("Environment variables for Event Hub are not set.")
+# if not eventhub_name or not eventhub_string:
+#     raise ValueError("Environment variables for Event Hub are not set.")
 # Load config
 
 # Initialize Event Hub producer
